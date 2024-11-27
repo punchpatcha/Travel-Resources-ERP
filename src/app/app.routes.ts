4eccfd43d7184bd71de607f6470b8548031ee754
Attjar <sanzab2468@gmail.com>
2024-11-28 04:42:21 +0700

    add-item UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { ResourceComponent } from './resource/resource.component';
 import { BookingComponent } from './booking/booking.component';
 import { AddBookingComponent } from './add-booking/add-booking.component';
+import { AddVehiclesComponent } from './add-vehicles/add-vehicles.component';
+import { AddEquipmentComponent } from './add-equipment/add-equipment.component';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: 'resource', pathMatch: 'full' }, // ตั้งหน้าเริ่มต้นเป็น Resource
   { path: 'resource', component: ResourceComponent },
   { path: 'booking', component: BookingComponent },
   { path: 'booking/add', component: AddBookingComponent },
+  { path: 'vehicles/add', component: AddVehiclesComponent },
+  { path: 'equipment/add', component: AddEquipmentComponent },
 ];
